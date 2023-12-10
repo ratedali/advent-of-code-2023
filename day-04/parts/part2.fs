77180cab4173c0294ed025493b60aece953d90e1
Ali Salaheldin <rated.ali.7@gmail.com>
2023-12-10 13:51:18 +0100

    day 04 part 1

--- day-04/parts/part2.fs
+++ day-04/parts/part2.fs
@@ -3,5 +3,28 @@ module day_04.parts.part2
 open day_04
 open utils
 open parse
+open day_04.parts
 
-let solve (data: Input): int = 0
+let init (cards: Card seq) =
+    cards |> Seq.map (fun card -> card, 1) |> List.ofSeq
+
+let processCards (cards: Card seq) =
+    let rec iter (cards: (Card * int) list) (acc: (Card * int) list) =
+        match cards with
+        | [] -> acc
+        | (card, dupes) as current :: cards ->
+            let wins = card |> part1.winningSet |> Set.count
+
+            let (toClone, rest) =
+                if List.length cards < wins then
+                    cards, []
+                else
+                    List.splitAt wins cards
+
+            let cloned = toClone |> List.map (fun (card, count) -> card, count + dupes)
+
+            iter (cloned @ rest) (current :: acc)
+
+    iter (init cards) []
+
+let solve (data: Input) : int = data |> processCards |> Seq.sumBy snd
